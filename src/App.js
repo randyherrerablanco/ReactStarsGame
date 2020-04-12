2905009e109897e152ccdc01d568cd162f59378a
Randy Herrera Blanco <randyherrerablanco@gmail.com>
2020-04-12 15:02:24 -0500

    Game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game';
+
+
 
 function App() {
+  const [gameId, setGameId] = React.useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameId}
+      <Game key={gameId} newGame={() => {setGameId(gameId + 1)}}/>
     </div>
   );
 }
 
 export default App;
+ 
